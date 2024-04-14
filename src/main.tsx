a14c2adfaefbaa34f5fab2cbc9d87a3d19ed36e7
Segunmaru Faozan <segunfaozan@gmail.com>
2024-04-14 19:44:03 +0100

    prettier

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,20 @@ import App from "./App.tsx";
 
 import { BrowserRouter } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
